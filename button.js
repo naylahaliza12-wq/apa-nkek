651363bb732d38643bebaf33eaf1a944898cef38
naylahaliza12-wq <naylahaliza12@gmail.com>
2025-08-22 11:26:35 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,3 @@
+function username(){
+            alert("Nama Lengkap: Nayla haliza \nHobi: memasak \nUmur: 19 \nNama Sekolah: Smk Media Karya \nTTL: Jakarta 03 juli 2006 ");
+        }
---
+++ divid.js
@@ -0,0 +1,5 @@
+//membuat objek elemen
+      var nama = document.getElementById("tujuan");
+      
+      // menampilkan output ke elemen hasil
+      nama.innerHTML = "<p>keinginan :  nonton konser bts & Punya kerjaan yang bagus <br>Hobi : memasak and membaca novel <br>Tujuan Hidup : agar tetap hidup dengan semestinya </p>";
